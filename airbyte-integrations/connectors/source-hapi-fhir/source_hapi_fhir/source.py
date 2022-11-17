241d00889155865d65447fd119d61fafd54a465e
Kassim Sheghembe <sheghembekassim@gmail.com>
2022-11-17 11:57:36 +0300

    Updated source and stream to include HTSIndexUntestedStream.

--- airbyte-integrations/connectors/source-hapi-fhir/source_hapi_fhir/source.py
+++ airbyte-integrations/connectors/source-hapi-fhir/source_hapi_fhir/source.py
@@ -11,7 +11,7 @@ from airbyte_cdk.models import SyncMode
 from airbyte_cdk.sources.streams.http.auth import TokenAuthenticator
 from keycloak import KeycloakOpenID
 
-from .streams import Patient, HivTestTestedPositive, CurrentOnArtStream
+from .streams import Patient, HivTestTestedPositive, CurrentOnArtStream, HtsIndexStream, HtsIndexUntestedStream
 
 """
 TODO: Most comments in this class are instructive and should be deleted after the source is implemented.
@@ -68,4 +68,8 @@ class SourceHapiFhir(AbstractSource):
         token = keycloak_openid.token(username=config["username"], password=config["password"])
 
         auth = TokenAuthenticator(token=token["access_token"])  # Oauth2Authenticator is also available if you need oauth support
-        return [Patient(authenticator=auth), HivTestTestedPositive(authenticator=auth), CurrentOnArtStream(authenticator=auth)]
+        return [Patient(authenticator=auth),
+                HivTestTestedPositive(authenticator=auth),
+                CurrentOnArtStream(authenticator=auth),
+                HtsIndexStream(authenticator=auth),
+                HtsIndexUntestedStream(authenticator=auth)]
