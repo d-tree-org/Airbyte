ea7a2bfbbccca0027c461a02bb2b8152ac0ba08c
evance-mose <evasmose@gmail.com>
2024-04-25 09:08:05 +0200

    pull organization affiliation data

--- airbyte-integrations/connectors/source-hapi-fhir/source_hapi_fhir/streams.py
+++ airbyte-integrations/connectors/source-hapi-fhir/source_hapi_fhir/streams.py
@@ -1160,3 +1160,47 @@ class Encounter(IncrementalHapiFhirStream, ABC):
         else:
             params.update(next_page_token)
             return params
+
+class OrganizationAffiliation(IncrementalHapiFhirStream, ABC):
+    primary_key = None
+
+    def path(
+            self,
+            *,
+            stream_state: Mapping[str, Any] = None,
+            stream_slice: Mapping[str, Any] = None,
+            next_page_token: Mapping[str, Any] = None,
+    ) -> str:
+        if next_page_token is None:
+            return "OrganizationAffiliation/_search"
+        else:
+            ""
+
+    def parse_response(self, response: requests.Response, **kwargs) -> Iterable[Mapping]:
+        response_json = response.json()
+
+        if 'entry' in response_json:
+            for location in response_json['entry']:
+                yield location
+        else:
+            pass
+
+    def request_params(self, stream_state: Mapping[str, Any], stream_slice: Mapping[str, any] = None,
+                       next_page_token: Mapping[str, Any] = None) -> MutableMapping[str, Any]:
+        params = {}
+        if stream_state:
+            last_updated_timestamp = stream_state.get(self.cursor_field)
+            # Hardcoded ZoneInfo, the FHIR server ZoneInfo to make sure that you have the real time for lastUpdated params
+            last_updated = datetime.datetime.fromtimestamp(last_updated_timestamp, ZoneInfo("Africa/Blantyre"))
+            last_updated_date = last_updated.strftime("%Y-%m-%dT%H:%M:%S.%f")
+            last_updated_date_params = {"_lastUpdated": "gt" + last_updated_date}
+            print("#################################" + last_updated_date)
+            params.update(last_updated_date_params)
+        if next_page_token is None:
+            organization_affiliation_param = {"active": "true", "_count": "500"}
+            params.update(organization_affiliation_param)
+            return params
+        else:
+            params.update(next_page_token)
+            return params
+
