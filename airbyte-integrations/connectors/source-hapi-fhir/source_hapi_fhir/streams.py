4ca437d1847dfb13af2599a72bd66f9b8c937624
Evance <56567958+evance-mose@users.noreply.github.com>
2024-12-10 11:15:30 +0200

    add contacts resources to pull data (#29)

--- airbyte-integrations/connectors/source-hapi-fhir/source_hapi_fhir/streams.py
+++ airbyte-integrations/connectors/source-hapi-fhir/source_hapi_fhir/streams.py
@@ -1454,3 +1454,53 @@ class ExposedInfantRecordHivTestResults(IncrementalHapiFhirStream, ABC):
         else:
             params.update(next_page_token)
             return params
+        
+class HivTestAndNextAppt(IncrementalHapiFhirStream, ABC):
+    def __init__(self, url: str, **kwargs):
+        super(HivTestAndNextAppt, self).__init__(url, **kwargs)
+        self.resources_config = resources_config
+        self.tags_to_remove = self.resources_config.get('otherResource', [])
+        self.link_ids_to_keep = self.resources_config.get('questionnaireResponse',{}).get('hiv-test-next-appt',{})
+        
+    def path(
+            self,
+            *,
+            stream_state: Mapping[str, Any] = None,
+            stream_slice: Mapping[str, Any] = None,
+            next_page_token: Mapping[str, Any] = None,
+    ) -> str:
+        if next_page_token is None:
+            return "QuestionnaireResponse/_search"
+        else:
+            ""
+
+    def parse_response(self, response: requests.Response, **kwargs) -> Iterable[Mapping]:
+        response_json = response.json()
+
+        if 'entry' in response_json:
+            for questionnaire_response in response_json['entry']:
+                yield process_data(questionnaire_response, self.tags_to_remove, self.link_ids_to_keep)
+        else:
+            pass
+
+    primary_key = None
+
+    def request_params(
+            self, stream_state: Mapping[str, Any], stream_slice: Mapping[str, any] = None, next_page_token: Mapping[str, Any] = None
+    ) -> MutableMapping[str, Any]:
+        params = {}
+        if stream_state:
+            last_updated_timestamp = stream_state.get(self.cursor_field)
+            # Hardcoded ZoneInfo, the FHIR server ZoneInfo to make sure that you have the real time for lastUpdated params
+            last_updated = datetime.datetime.fromtimestamp(last_updated_timestamp, ZoneInfo("Africa/Blantyre"))
+            last_updated_date = last_updated.strftime("%Y-%m-%dT%H:%M:%S.%f")
+            last_updated_date_params = {"_lastUpdated": "gt" + last_updated_date}
+            print("#################################" + last_updated_date)
+            params.update(last_updated_date_params)
+        if next_page_token is None:
+            questionnaire_param = {"questionnaire": "Questionnaire/contact-and-community-positive-hiv-test-and-next-appointment", "_count": "500"}
+            params.update(questionnaire_param)
+            return params
+        else:
+            params.update(next_page_token)
+            return params
